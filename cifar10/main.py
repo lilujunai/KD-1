927ac50d2445c3b24e30020d8afe91bacc450baf
diziOh <34847452+ooodragon94@users.noreply.github.com>
2020-07-11 15:51:06 +0900

    cifar: progress bar fix

--- cifar10/main.py
+++ cifar10/main.py
@@ -155,7 +155,7 @@ else:
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %5.3f%% (%d/%d)'
+            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %5.2f%% (%d/%d)'
                          % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
         scheduler.step()
         return 100.*correct/total
