ceae452843cf556f88021e4253ed75cfa7585ed4
diziOh <34847452+ooodragon94@users.noreply.github.com>
2020-07-12 14:03:35 +0900

    minor fix

--- cifar10/main.py
+++ cifar10/main.py
@@ -130,7 +130,7 @@ if args.kd:
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %5.2f%% (%d/%d)'
+            progress_bar(batch_idx, len(trainloader), 'Loss: {:.3f} | Acc: {:4.3f} (%d/%d)'
                          % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
         scheduler.step()
         return 100. * correct / total
